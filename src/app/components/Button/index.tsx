b5dc65fe4a1e3ab86749d9188389e8ee3d4fc759
magdaavram <magdahalus@gmail.com>
2020-04-24 15:59:43 +0300

    Create button component

---
+++ src/app/components/Button/index.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  width: 50px;
+  height: 50px;
+  padding: 0;
+  border: none;
+  outline: none;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+export default Button;
--- src/app/components/TopControls/index.tsx
+++ src/app/components/TopControls/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import Icons from "../Icons";
+import Button from '../Button';
+import AddIcon from '../../icons/Add';
+import DeleteIcon from '../../icons/Delete';
+import EditIcon from '../../icons/Edit';
+import styled from 'styled-components';
+
+const ButtonWithIcon = styled(Button)`
+  margin-left: 9px;
+  border-radius: 50%;
+
+  :hover {
+    opacity: 0.9;
+  }
+`;
 
 const TopControls = () => (
-	<div>
-		<Icons />
-	</div>
+  <div>
+    <ButtonWithIcon onClick={() => console.log('pressed add')}>
+      <AddIcon />
+    </ButtonWithIcon>
+    <ButtonWithIcon onClick={() => console.log('pressed delete')}>
+      <DeleteIcon />
+    </ButtonWithIcon>
+    <ButtonWithIcon onClick={() => console.log('pressed edit')}>
+      <EditIcon />
+    </ButtonWithIcon>
+  </div>
 );
 
 export default TopControls;
