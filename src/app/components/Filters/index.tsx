5b57ede874ca69859ad969d7cc4ee7827a7cf86a
magdaavram <magdahalus@gmail.com>
2020-04-25 08:40:43 +0300

    Refactor tag component as a single element

--- src/app/components/Filters/index.tsx
+++ src/app/components/Filters/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Tag from '../Tag';
 import styled from 'styled-components';
 
-const StyledWrapper = styled.div`
+const Container = styled.div`
   width: 70%;
   margin: 18px;
   background-color: rgba(201, 173, 167, 0.3);
@@ -10,21 +10,28 @@ const StyledWrapper = styled.div`
   border-radius: 12px;
 `;
 
-const StyledHeader = styled.h3`
+const Header = styled.h3`
   padding: 0;
   margin: 0 0 18px 0;
   color: var(--color--light-purple);
   font-size: var(--font-size--large);
 `;
 
+const TagContainer = styled.div`
+  flex: flex wrap;
+`;
+
 const Filters = () => {
   return (
-    <StyledWrapper>
-      <StyledHeader>Recommended ingredients</StyledHeader>
-      <div>
-        <Tag />
-      </div>
-    </StyledWrapper>
+    <Container>
+      <Header>Recommended ingredients</Header>
+      <TagContainer>
+        <Tag name={'tomatoes'} active={true} />
+        <Tag name={'potatoes'} active={false} />
+        <Tag name={'garlic'} active={true} />
+        <Tag name={'spinach'} active={false} />
+      </TagContainer>
+    </Container>
   );
 };
 
--- src/app/components/Tag/index.tsx
+++ src/app/components/Tag/index.tsx
@@ -2,16 +2,13 @@ import React, { InputHTMLAttributes } from 'react';
 import styled from 'styled-components';
 
 interface Props extends InputHTMLAttributes<HTMLDivElement> {
-  active?: boolean;
+  name: string;
+  active: boolean;
 }
 
-const tagProps: Props = {
-  active: true,
-};
-
-const StyledTag = styled.div((props: Props) => ({
+const TagContainer = styled.div((props: Props) => ({
   display: 'inline-block',
-  marginRight: '18px',
+  margin: '0 0 12px 12px',
   padding: '6px 12px 6px 12px',
   borderRadius: '9px',
   backgroundColor: props.active ? 'rgba(110, 37, 52, 0.7)' : 'rgba(110, 37, 52, 0.3)',
@@ -20,25 +17,11 @@ const StyledTag = styled.div((props: Props) => ({
   cursor: 'pointer',
 }));
 
-const Tag = () => {
+const Tag = (props: Props) => {
   return (
-    <>
-      <StyledTag {...tagProps} onClick={() => console.log('pressed tomatoes')}>
-        <span>tomatoes</span>
-      </StyledTag>
-
-      <StyledTag onClick={() => console.log('pressed potatoes')}>
-        <span>potatoes</span>
-      </StyledTag>
-
-      <StyledTag {...tagProps} onClick={() => console.log('pressed garlic')}>
-        <span>garlic</span>
-      </StyledTag>
-
-      <StyledTag onClick={() => console.log('pressed spinach')}>
-        <span>spinach</span>
-      </StyledTag>
-    </>
+    <TagContainer {...props} onClick={() => console.log(`pressed ${props.name}`)}>
+      <span>{props.name}</span>
+    </TagContainer>
   );
 };
 
