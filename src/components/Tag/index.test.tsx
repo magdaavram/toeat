22a8474a76325f5fbdcba26dbfec6b4d61236c0c
magdaavram <magdahalus@gmail.com>
2020-05-15 19:33:45 +0300

    Add onClick test for Tag component

--- src/components/Tag/index.test.tsx
+++ src/components/Tag/index.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { create } from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import Tag from './index';
 
 it('should render as active', () => {
@@ -12,4 +13,12 @@ it('should render as inactive', () => {
   const tree = create(<Tag name={'tomato'} active={false} />);
 
   expect(tree.toJSON()).toMatchSnapshot();
+});
+
+it('should call the onClick function', () => {
+  const onClick = jest.fn();
+  const wrapper = shallow(<Tag name={'tomato'} active={true} onClick={onClick} />);
+
+  wrapper.props().onClick();
+  expect(onClick).toHaveBeenCalledTimes(1);
 });
