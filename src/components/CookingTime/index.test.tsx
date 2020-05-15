199d4128d83459df6b87b4053966b7380a21bd31
magdaavram <magdahalus@gmail.com>
2020-05-15 15:19:50 +0300

    Fix tests and typo in CookingTime component

--- src/components/CookingTime/index.test.tsx
+++ src/components/CookingTime/index.test.tsx
@@ -6,30 +6,30 @@ it('should render with icon and text', () => {
   const wrapper = mount(<CookingTime duration={30} hasIcon={true} />);
 
   expect(wrapper.find('svg')).toHaveLength(1);
-  expect(wrapper.contains('30 minutes'));
+  expect(wrapper.text()).toEqual('30 min');
 });
 
 it('should render with text only', () => {
   const wrapper = shallow(<CookingTime duration={30} hasIcon={false} />);
 
   expect(wrapper.find('DurationIcon')).toHaveLength(0);
-  expect(wrapper.contains('30 minutes'));
+  expect(wrapper.text()).toEqual('30 min');
 });
 
 it('should render with minutes only (less than 60)', () => {
   const wrapper = shallow(<CookingTime duration={10} hasIcon={false} />);
 
-  expect(wrapper.contains('10 minutes'));
+  expect(wrapper.text()).toEqual('10 min');
 });
 
 it('should render with hours only (multiple of 60)', () => {
   const wrapper = shallow(<CookingTime duration={120} hasIcon={false} />);
 
-  expect(wrapper.contains('2 hours'));
+  expect(wrapper.text()).toEqual('2h');
 });
 
 it('should render with hours and minutes (more than 60 and not multiple of 60)', () => {
   const wrapper = shallow(<CookingTime duration={210} hasIcon={false} />);
 
-  expect(wrapper.contains('3 hours 30 minutes'));
+  expect(wrapper.text()).toEqual('3h 30 min');
 });
