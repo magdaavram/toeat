93dbd2f311f9300c445e41581859da96acf9e1df
magdaavram <magdahalus@gmail.com>
2021-02-21 22:18:12 +0200

    Increase post limit

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ client.connect(function(err) {
   console.log("Connected successfully to server");
 });
 
-app.use(express.json());
+app.use(express.json({limit: '5mb'}));
 
 
 app.get('/recipes', (req, res) => {
