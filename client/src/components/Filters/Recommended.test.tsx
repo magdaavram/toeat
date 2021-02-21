e8fb94370c426de2e64bf2c2b73687017cfbbad9
magdaavram <magdahalus@gmail.com>
2021-02-21 17:22:09 +0200

    Test recommended component renders correctly

---
+++ client/src/components/Filters/Recommended.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import Recommended from './Recommended';
+
+it('should render correctly', () => {
+  const wrapper = mount(<Recommended />);
+
+  expect(wrapper).toMatchSnapshot();
+});
