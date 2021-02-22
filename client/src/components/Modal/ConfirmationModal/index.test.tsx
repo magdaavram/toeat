365ac6abe6c88bdb9fb52b60da6911e24f8a7cb0
magdaavram <magdahalus@gmail.com>
2021-02-22 19:34:30 +0200

    Test confirmation modal component

---
+++ client/src/components/Modal/ConfirmationModal/index.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import ConfirmationModal from './index';
+
+describe('<ConfirmationModal />', () => {
+  const onClose = jest.fn();
+  const onConfirm = jest.fn();
+  const props = {
+    title: 'Title',
+    message: 'Message',
+    show: true,
+    onClose,
+    onConfirm,
+  };
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<ConfirmationModal {...props}/>);
+  });
+
+  it('should render correctly when shown', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render correctly when not shown', () => {
+    const newProps = {
+      ...props,
+      show: false
+    };
+    wrapper = mount(<ConfirmationModal {...newProps} />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
